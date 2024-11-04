b4b357033e4d0d7376730f8fb2b9c1a273a4f59d
Trevor <trevor.wh113@gmail.com>
2024-11-04 15:32:19 -0500

    TW: Changed some test cases to not verify the database side to avoid duplication with other test cases being developed

--- app/tests/test_prescription_creation.py
+++ app/tests/test_prescription_creation.py
@@ -26,9 +26,8 @@ def test_load_clients_create_page():
 def test_submit_with_valid_data():
     """
     Success case: Tests that a submission redirects to the client page when a
-    valid DIN is entered. Also makes sure database is updated accordingly (POST).
-    Note that this test should result in a client "JOHN DOE" getting a new
-    prescription.
+    valid DIN is entered (POST). Note that this test should result in a client 
+    "JOHN DOE" getting a new prescription. Does NOT check the database.
     """
     # Create a distinct new prescription.
     target = f"Dr. {random.randint(0,1000000)}"
@@ -40,23 +39,12 @@ def test_submit_with_valid_data():
     assert b"Client Page" in response.data, "Did not load client page correctly"
     assert bytes(target, 'utf-8') in response.data, "Did not load new prescription correctly"
 
-    # Check that the prescription was added to the database.
-    dbname = database.get_database()    
-    client = dbname["clients"].find_one({"phone_number": "(123)-456-7890"})
-    client_actives = client["active_pres"]
-    found = False
-    for pres in client_actives:
-        if target in pres:
-            found = True
-            break
-    assert found, "Did not update database correctly with new prescription"
-
 # Failure cases for /clients/(123)-456-7890/create route ------------------------------
 def test_submit_with_invalid_din():
     """
     Failure case: Tests that a submission remains on the prescription creation
-    page when given an invalid DIN (not an integer). Also ensures database is
-    not updated (POST).
+    page when given an invalid DIN (not an integer) (POST). Does NOT check the 
+    database.
     """
     # Create an invalid test prescription.
     data = {"DIN": "", "preBy": "NOT A DOCTOR", "interval": "01/01/2025"}
@@ -66,22 +54,11 @@ def test_submit_with_invalid_din():
     assert response.status_code == 200
     assert b"Prescription Creation" in response.data, "Did not reload prescription creation page correctly"
 
-    # Check that the prescription was NOT added to the database.
-    dbname = database.get_database()    
-    client = dbname["clients"].find_one({"phone_number": "(123)-456-7890"})
-    client_actives = client["active_pres"]
-    not_found = True
-    for pres in client_actives:
-        if "NOT A DOCTOR" in pres:
-            not_found = False
-            break
-    assert not_found, "Wrongly updated the database with new prescription"
-
 def test_submit_with_inexistant_din():
     """
     Failure case: Tests that a submission remains on the prescription creation
-    page when given an inexsitent DIN (not in the drug database). Also ensures 
-    the client database is not updated (POST).
+    page when given an inexsitent DIN (not in the drug database) (POST). Does NOT 
+    check the database.
     """
     # Create a test prescription with inexistent din.
     data = {"DIN": "000000000", "preBy": "NOT A DRUG", "interval": "01/01/2025"}
@@ -90,14 +67,3 @@ def test_submit_with_inexistant_din():
     response = app.test_client().post('/clients/(123)-456-7890/create', data=data)
     assert response.status_code == 200
     assert b"Prescription Creation" in response.data, "Did not reload prescription creation page correctly"
-
-    # Check that the prescription was NOT added to the database.
-    dbname = database.get_database()    
-    client = dbname["clients"].find_one({"phone_number": "(123)-456-7890"})
-    client_actives = client["active_pres"]
-    not_found = True
-    for pres in client_actives:
-        if "NOT A DRUG" in pres:
-            not_found = False
-            break
-    assert not_found, "Wrongly updated the database with new prescription"
