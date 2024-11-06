ab644e29c016ebfc73bd7a91e765a9459c86f2a8
spagazani <sophia.n.pagazani@gmail.com>
2024-11-06 15:31:15 -0500

    SP: Removed useless code from database.py

--- app/database.py
+++ app/database.py
@@ -13,8 +13,3 @@ def get_database():
 
     # Create the database for our example (we will use the same database throughout the tutorial
     return client['medicine_mgmt_data']
-
-# This is added so that many files can reuse the function get_database()
-if __name__ == "__main__":   
-    # Get the database
-    dbname = get_database()
