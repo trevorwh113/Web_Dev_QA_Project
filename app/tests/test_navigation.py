d7683405707c0d184cbd5ca13aec69157dc89f03
Trevor <trevor.wh113@gmail.com>
2024-11-21 09:42:48 -0500

    TW: Created navigation test case + modified some page templates to enable this

---
+++ app/tests/test_navigation.py
@@ -0,0 +1,39 @@
+"""
+This test file implements an integration/end-to-end test for
+the html button navigation between pages.
+"""
+
+from flask import url_for
+from playwright.sync_api import expect
+import re
+
+def test_home_and_back_buttons(live_server, page):    
+    # Load the home page and make sure it comes up.
+    page.goto(url_for('home', _external=True))
+    expect(page).to_have_title(re.compile("Home"))
+    
+    # Navigate through the client pages.
+    page.get_by_role("button", name="Manage Client Prescriptions").click()
+    expect(page).to_have_title(re.compile("Manage Client Prescriptions"))
+    page.get_by_role("button", name="JOHN DOE").click()
+    expect(page).to_have_title(re.compile("Client Page"))
+    page.get_by_role("button", name="Create Prescription").click()
+    expect(page).to_have_title(re.compile("Prescription Creation"))
+    
+    # Naviguate back to the home page.
+    page.get_by_role("button", name="Cancel").click()
+    expect(page).to_have_title(re.compile("Client Page"))
+    page.get_by_test_id("back").click()
+    expect(page).to_have_title(re.compile("Manage Client Prescriptions"))
+    page.get_by_test_id("back").click()
+    expect(page).to_have_title(re.compile("Home"))
+
+    # Naviguate through the inventory page and back.
+    page.get_by_role("button", name="Search Inventory").click()
+    expect(page).to_have_title(re.compile("Search Inventory"))
+    page.get_by_test_id("back").click()
+    expect(page).to_have_title(re.compile("Home"))
+
+    # Naviguate to the supply page.
+    page.get_by_role("button", name="Manage Supply Orders").click()
+    expect(page).to_have_title(re.compile("Unavailable"))
